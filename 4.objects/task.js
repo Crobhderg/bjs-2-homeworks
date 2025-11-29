89fad68a56a82c9e3ecf2b0a894c1c9624ff5045
Crobh derg <safr0novada@yandex.ru>
2025-11-29 14:57:10 +0500

    Готово

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -1,19 +1,35 @@
 function Student(name, gender, age) {
-  
+	this.name = name;
+	this.gender = gender;
+	this.age = age;
+	this.marks = [];
 }
 
-Student.prototype.setSubject = function (subjectName) {
-  
-}
+// Установка предмета
+Student.prototype.setSubject = function(subjectName) {
+	this.subject = subjectName;
+};
 
-Student.prototype.addMarks = function (...marks) {
-  
-}
+// Добавление оценок
+Student.prototype.addMarks = function(...marksToAdd) {
+	if (!this.marks) {
+		// Студент отчислен — у него нет массива marks
+		return;
+	}
+	this.marks.push(...marksToAdd);
+};
 
-Student.prototype.getAverage = function () {
-  
-}
+// Средний балл
+Student.prototype.getAverage = function() {
+	if (!this.marks || this.marks.length === 0) {
+		return 0;
+	}
+	return this.marks.reduce((sum, m) => sum + m, 0) / this.marks.length;
+};
 
-Student.prototype.exclude = function (reason) {
-  
-}
+// Отчисление
+Student.prototype.exclude = function(reason) {
+	delete this.subject;
+	delete this.marks;
+	this.excluded = reason;
+};
